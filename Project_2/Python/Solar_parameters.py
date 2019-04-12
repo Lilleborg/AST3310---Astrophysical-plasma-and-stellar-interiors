ecdc8406932480fde23d0ccfa127c3e9fe036e72
Jakob Borg <borg.jakob@gmail.com>
2019-04-12 23:31:13 +0200

    Adding opacity and solar_parameters files

---
+++ Project_2/Python/Solar_parameters.py
@@ -0,0 +1,16 @@
+'''
+File containing known parameters from the Sun
+'''
+
+## General parameters
+L = 3.846e26        # [W]
+M = 1.989e30        # [Kg]
+R = 6.96e8          # [m]
+lifetime = 4.57e9   # [years]
+kappa = 1.0         # [m^2 kg^-1]
+rho_avg = 1.408e3   # [kg m^-3]
+
+## Core parameters
+rho_core = 1.62e5   # [kg m^-3]
+T_core = 1.57e7     # [K]
+P_core = 3.45e16    # [Pa]
